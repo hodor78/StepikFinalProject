70fc977e78801fc11885c876a0e95ef92d39dcad
hodor78 <hodor1978@gmail.com>
2022-11-04 13:37:57 -0400

    login_page tests were added to test_main_page.py

--- test_main_page.py
+++ test_main_page.py
@@ -13,3 +13,27 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, url)
     page.open()
     page.should_be_login_link()
+
+def test_should_be_login_url_present_for_guest(browser):
+    url = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, url)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_url()
+
+def test_should_be_login_form_present_for_guest(browser):
+    url = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, url)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_form()
+
+def test_should_be_register_form_present_for_guest(browser):
+    url = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, url)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_register_form()
