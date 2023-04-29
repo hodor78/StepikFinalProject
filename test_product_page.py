9357b1fde412ddb61dcb585c6cf194e19462e06d
hodor78 <hodor1978@gmail.com>
2023-04-29 08:54:23 -0400

    final cleansing before peer review; need_review markwas added

--- test_product_page.py
+++ test_product_page.py
@@ -89,7 +89,6 @@ def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     page = ProductPage(browser, url)
     page.open()
     page.add_to_basket()
-    page.solve_quiz_and_get_code()
     page.should_not_be_success_message()
 
 def test_guest_cant_see_success_message(browser):
@@ -104,7 +103,6 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page = ProductPage(browser, url)
     page.open()
     page.add_to_basket()
-    page.solve_quiz_and_get_code()
     page.should_disappear_success_massege()
 
 def test_guest_should_see_login_link_on_product_page(browser):
