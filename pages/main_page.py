60e420615cb4f1b5cd93cdc30adcc4200b2104d3
hodor78 <hodor1978@gmail.com>
2023-04-29 13:20:05 -0400

    final cleansing before peer review; need_review markwas added

--- pages/main_page.py
+++ pages/main_page.py
@@ -4,7 +4,8 @@ from selenium.webdriver.common.by import By
 
 
 class MainPage(BasePage):
-    def __init__(self, *args, **kwargs):
-        super(MainPage, self).__init__(*args, **kwargs)
+    pass
+    # def __init__(self, *args, **kwargs):
+    #     super(MainPage, self).__init__(*args, **kwargs)
 
 
